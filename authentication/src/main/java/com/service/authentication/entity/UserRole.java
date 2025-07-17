0d364f53f7f560c212ca4c233474c2d88550175b
dipa.alhaza <dipa.alhaza@ui.ac.id>
2025-07-17 23:34:39 +0700

    chore: add user role seeder

--- authentication/src/main/java/com/service/authentication/entity/UserRole.java
+++ authentication/src/main/java/com/service/authentication/entity/UserRole.java
@@ -1,7 +1,18 @@
 package com.service.authentication.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Builder;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,20 +20,22 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "user_roles")
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Builder
-public class UserRole {
+public class UserRole extends BaseEntity {
 
     @Id
     @GeneratedValue
     private UUID id;
 
+    @Column(nullable = false, unique = true)
+    private String name;
+    
     @OneToMany(mappedBy = "userRole", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private Set<UserRoleAssignment> assignments = new HashSet<>();
 
-    @Column(nullable = false, unique = true)
-    private String name;
 }
--- authentication/src/main/java/com/service/authentication/repository/UserRoleRepository.java
+++ authentication/src/main/java/com/service/authentication/repository/UserRoleRepository.java
@@ -1,11 +1,23 @@
 package com.service.authentication.repository;
 
 import com.service.authentication.entity.UserRole;
+
+import io.lettuce.core.dynamic.annotation.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, UUID> {
     Optional<UserRole> findByName(String name);
+    Optional<UserRole> findById(UUID id);
+    void deleteById(UUID id);
+
+    @Modifying
+    @Query("UPDATE User u SET u.deletedAt = CURRENT_TIMESTAMP WHERE u.id = :id")
+    void softDeleteById(@Param("id") UUID id);
+    
 }
