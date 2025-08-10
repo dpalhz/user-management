789c5e0f5b4222aa4976b7e2ac0dc90e0fdf1c70
dipa.alhaza <dipa.alhaza@ui.ac.id>
2025-08-10 10:27:43 +0700

    feat: add custom user details for spring security authentication

--- authentication/src/main/java/com/service/authentication/security/CustomUserDetailsService.java
+++ authentication/src/main/java/com/service/authentication/security/CustomUserDetailsService.java
@@ -3,13 +3,16 @@ package com.service.authentication.security;
 import com.service.authentication.entity.User;
 import com.service.authentication.entity.UserRoleAssignment;
 import com.service.authentication.repository.UserRepository;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,24 +20,23 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   private final UserRepository userRepository;
 
+  @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user =
-        userRepository
-            .findWithRolesByUsername(username)
-            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    List<GrantedAuthority> authorities =
-        user.getRoleAssignments().stream()
-            .map(UserRoleAssignment::getUserRole)
-            .map(userRole -> new SimpleGrantedAuthority("ROLE_" + userRole.getName()))
-            .collect(Collectors.toList());
+    User user = userRepository
+        .findWithRolesByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-    return new org.springframework.security.core.userdetails.User(
-        user.getId().toString(),
+    List<GrantedAuthority> authorities = user.getRoleAssignments().stream()
+        .map(UserRoleAssignment::getUserRole)
+        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+        .collect(Collectors.toList());
+
+    return new CustomUserDetails(
+        user.getId(),
+        user.getUsername(),
         user.getPassword(),
-        user.isEnabled(), // enabled
-        true, // accountNonExpired
-        true, // credentialsNonExpired
-        true, // accountNonLocked
-        authorities);
+        user.isEnabled(),
+        authorities
+    );
   }
 }
